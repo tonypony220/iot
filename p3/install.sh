c4d53c254e793914179617fab26f7dfd35f183e0
tonypony220 <tony8pony@gmail.com>
2025-10-28 11:04:23 +0100

    works

--- p3/install.sh
+++ p3/install.sh
@@ -1,39 +1,49 @@
 # Install k3d (tiny Kubernetes-in-Docker). Easier & faster than full K8s for a demo.
 curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
 
+#sudo k3d cluster delete demo
+
 # Create a single-node cluster named "demo".
 # --agents 1 adds one worker (optional but realistic).
-# We also map HTTP 80 of the cluster LB to localhost:8080 so it's easy to reach apps later.
+# We also map HTTP 80 of the cluster LB to localhost:8888 so it's easy to reach apps later.
 # -p/--port exposes Service LB ports via the k3d-proxy container
 # --api-port pins the host port for the Kubernetes API (6443 inside cluster)
-sudo k3d cluster create demo \
+k3d cluster create demo \
   --servers 1 --agents 1 \
   --api-port 6445 \
-  --port "8080:80@loadbalancer"
+      -p "8080:80@loadbalancer" \
+  --port "8888:8888@loadbalancer"
 
 # Point kubectl at this new cluster (k3d creates/updates the kubeconfig for you).
-sudo kubectl cluster-info
+kubectl cluster-info
 
 # System namespace for Argo CD components.
-sudo kubectl create namespace argocd
+kubectl create namespace argocd
 
 # Application namespace where your app Pods/Services will live.
-sudo kubectl create namespace dev
+kubectl create namespace dev
 
 # Install the official Argo CD bundle (CRDs + controllers + API/UI server).
 # We install it *into* the argocd namespace we just created.
-sudo kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+kubectl -n argocd patch deploy argocd-server \
+  --type='json' \
+  -p='[{"op":"add","path":"/spec/template/spec/containers/0/args/-","value":"--insecure"}]'
+
+kubectl apply -f confs/argo-ingress.yaml
+kubectl apply -f confs/argocd-app.yaml
 
 # Watch Argo CD pods come up (Ctrl+C when all are Running/Ready).
-sudo kubectl get pods -n argocd -w
+kubectl get pods -n argocd -w
 
 # Forward the Argo CD web UI to localhost:8081 (no Ingress needed).
 #
-# port-forward:  opens a TCP tunnel via the Kubernetes API from your local machine to a Service/Pod inside the cluster
-# svc/argocd-server = target Service in argocd namespace
-# 8081:80 = bind local port 8081 → Service port 80
-# --address 0.0.0.0 lets you open it from your LAN; omit if you prefer localhost-only.
-sudo kubectl -n argocd port-forward svc/argocd-server 8081:80 --address 0.0.0.0
+# 		port-forward:  opens a TCP tunnel via the Kubernetes API from your local machine to a Service/Pod inside the cluster
+# 		svc/argocd-server = target Service in argocd namespace
+# 		8081:80 = bind local port 8081 → Service port 80
+# 		--address 0.0.0.0 lets you open it from your LAN; omit if you prefer localhost-only.
+#
+# sudo kubectl -n argocd port-forward svc/argocd-server 8081:80 --address 0.0.0.0
 #
 # ------------- SERVICE VS INGRESS -------------
 #
@@ -61,7 +71,7 @@ sudo kubectl -n argocd port-forward svc/argocd-server 8081:80 --address 0.0.0.0
 # 
 # Argo CD bootstraps an admin password in a Secret.
 # We extract and decode it for first login.
-sudo kubectl -n argocd \
+kubectl -n argocd \
   get secret argocd-initial-admin-secret \
   -o jsonpath='{.data.password}' | base64 -d; echo
 
